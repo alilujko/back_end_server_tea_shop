843d31f8fb3a6f13ef494c999e593459d286fcc0
alilujko <u.alilujko@gmail.com>
2026-09-17 19:26:12 +0300

    first askdlfj;alksdfjakl;sdjflk

---
+++ main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import json
+
+app = FastAPI()
+
+login_realy = "Ustym"
+password_realy = "password"
+
+def read(url):
+    with open(url, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    return data    
+
+def write(f, objct):
+    with open(f, 'w', encoding='utf-8') as file:
+        json.dump(objct, file, ensure_ascii=False, indent=4)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+class Register(BaseModel):
+    login: str
+    password: str
+
+class Change_data(BaseModel):
+    old_name: str
+    new_name: str
+    new_image: str
+    new_price: int
+
+@app.get('/')
+def main():
+    return {"message":"Вітаю вас на потужному сервер"}
+
+@app.get('/get_menu')
+def get_menu():
+    menu = read('database.json')
+    return menu
+
+@app.post('/register')
+def reg(data: Register):
+    if data.login == login_realy and data.password == password_realy:
+        return {"message":"true"}
+    else:
+        return {"message":"false"}
+
+@app.post('/change_data')
+def change_data(data: Change_data):
+    menu = read('database.json')
+    menu[data.old_name]["name"] = data.new_name
+    menu[data.old_name]["image"] = data.new_image
+    menu[data.old_name]["price"] = data.new_price
+    
+    write("database.json", menu)
+
+    return {"message":"добре пройшло"}
+
+if __name__ == '__main__':
+    uvicorn.run(app)
